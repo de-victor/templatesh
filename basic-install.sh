74836217837c3baaa1a406b6c1c9832495586a70
Emmanuel Victor <2ndrunner@gmail.com>
2022-06-30 16:15:28 -0300

    fixed text

--- basic-install.sh
+++ basic-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo script for depencencies
-echo "in 5 seconds it will begin the installation of the follow programs"
+echo "Script to install traning dependency for Ubuntu 20.04"
+echo "In 5 seconds it will begin the installation of the follow programs"
 echo "docker"
 echo "hit ctrl+c to abort this operation"
 echo "..."
@@ -13,6 +13,11 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 sudo apt update
+echo "Installing docker!\n\n"
 sudo apt install -y docker-ce
+echo "seting docker in user group"
 sudo usermod -aG docker $USER
+echo "\n\n\n\n"
 echo "DONE!!!"
+echo "for runing docker without sudo please exit and open the terminal again"
+echo "Thank you!"
