434ad68adb54923730c1a5dec7d770571e5a0424
Emmanuel Victor <2ndrunner@gmail.com>
2022-06-30 22:23:57 -0300

    using functions

--- basic-install.sh
+++ basic-install.sh
@@ -1,36 +1,61 @@
 #!/bin/bash
-echo "Script to install traning dependency for Ubuntu 20.04"
-echo "In 5 seconds it will begin the installation of the follow programs"
-echo "docker"
-echo "openJDK 8"
-echo "maven"
-echo "hit ctrl+c to abort this operation"
-echo "..."
-echo -e "\n"
-sleep 5
 
-echo "...begin installation..."
-echo "installing docker"
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt-cache policy docker-ce
-sudo apt update
-echo "Installing docker!\n\n"
-sudo apt install -y docker-ce
+programs=("docker-ce" "openjdk-8-jdk" "maven")
+
+init()
+
+initialMsg(){
+  x=0
+  echo "Script to install traning dependency for Ubuntu 20.04"
+  echo "In 5 seconds it will begin the installation of the follow programs"
+  while [$x != ${#programs[@]}]
+  do
+    echo "${programs[$x]}"
+    let "x = x +1"
+  done
+  echo "hit ctrl+c to abort this operation"
+  echo "..."
+  echo -e "\n"
+  sleep 5
+}
+
+setup(){
+  echo "setup"
+  sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+  apt-cache policy docker-ce
+  sudo apt update
+}
+
+install(){
+  x=0
+  echo "start installation"
+  while [$x != ${#programs[@]}]
+  do
+    echo "installing "${programs[$x]}" now"
+    sudo apt install -y "${programs[$x]}"
+    let "x = x +1"
+  done
+}
+
+posInstallation(){
+echo "pos installation"
 echo "seting docker in user group"
 sudo usermod -aG docker $USER
 echo "\n\n\n\n"
+}
 
-#JAVA8
-echo "installing openJDK8"
-sudo apt install -y openjdk-8-jdk
-echo "\n\n\n\n"
+end(){
+  echo "DONE!!!"
+  echo "for runing docker without sudo please exit and open the terminal again"
+  echo "Thank you!"
+}
 
-echo "installing maven"
-sudo apt install -y maven
-echo "\n\n\n\n"
-
-echo "DONE!!!"
-echo "for runing docker without sudo please exit and open the terminal again"
-echo "Thank you!"
+init(){
+  initialMsg()
+  setup()
+  install()
+  posInstallation()
+  end()
+}
