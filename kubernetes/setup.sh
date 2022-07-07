e4cd42f9235efff5ac1ec2de01b7bdf159b507f2
de-victor <12victor.ds@gmail.com>
2022-07-06 22:05:15 -0300

    kubernetes install

---
+++ kubernetes/setup.sh
@@ -0,0 +1,34 @@
+echo "Script to install kubernetes for ubuntu 20.04"
+echo "In 5 seconds it will begin the installation of the follow programs"
+echo "hit ctrl+c to abort this operation"
+echo "..."
+echo -e "\n"
+sleep 5
+
+echo "droping iptables configuration!"
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -F
+sudo iptables --flush
+
+echo "docker instalation"
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+echo "setting docker in user group"
+sudo usermod -aG docker $USER
+
+echo "kubernetes installation"
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
