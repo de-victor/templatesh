5b8e18d902e52108d16efee79d03b3912be6c97a
Emmanuel Victor <2ndrunner@gmail.com>
2022-07-07 21:07:26 -0300

    change start cluster

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -1,1 +1,7 @@
-sudo kubeadm init --config kubeadm-config.yaml
+ips = $1
+endPoint = $2
+CERTKEY=$(kubeadm certs certificate-key)
+echo $CERTKEY
+echo "trying to setting up kubernet cluster with apiserver="$ips" and endpoint="$endPoint
+
+sudo kubeadm init --config kubeadm-config.yaml --apiserver-cert-extra-sans=$ips --pod-network-cidr=10.32.0.0/12 --control-plane-endpoint=$endPoint --upload-certs --certificate-key=$CERTKEY
