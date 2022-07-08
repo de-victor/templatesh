96c69ed0974ad989974f233bb30ca7eb12a51048
Emmanuel Victor <2ndrunner@gmail.com>
2022-07-08 00:03:54 -0300

    last fix over nodes network

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -5,7 +5,7 @@ file=kubeadm-config.yaml
 
 if [ -z $endPoint ] ; then
   echo "Inform endPoint"
-  echo "ex: .\start.sh xx.xx.xx.xx"
+  echo "ex: .\start.sh 192.0.7.41"
 else
   #modify config yaml
   sed -i 's/controlPlaneEndpoint:.*/controlPlaneEndpoint: '$endPoint'/' $file
@@ -19,8 +19,6 @@ else
   sudo chown $(id -u):$(id -g) $HOME/.kube/config
   
   #seting pods network
-  kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')" --output=json
+  kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
-  #generate a new token
-  sudo kubeadm token create
 fi
