3e79a23b1c1c9abe8c980ae9ea6e53a43c551e65
Emmanuel Victor <2ndrunner@gmail.com>
2022-07-07 21:48:32 -0300

    changes on the beahving

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -1,7 +1,12 @@
-ips = $1
-endPoint = $2
-CERTKEY=$(kubeadm certs certificate-key)
-echo $CERTKEY
-echo "trying to setting up kubernet cluster with apiserver="$ips" and endpoint="$endPoint
+ips=$1
+endPoint=$2
 
-sudo kubeadm init --config kubeadm-config.yaml --apiserver-cert-extra-sans=$ips --pod-network-cidr=10.32.0.0/12 --control-plane-endpoint=$endPoint --upload-certs --certificate-key=$CERTKEY
+if [ -z $ips ] || [ -z $endPoint ] ; then
+  echo "Inform the ips and endPoint"
+  echo "ex: .\start.sh xx.xx.xx.xx,xx.xx.xx.xx zz.zz.zz.zz"
+else
+  CERTKEY=$(kubeadm certs certificate-key)
+  echo $CERTKEY &> certkey.key
+  echo "trying to setting up kubernet cluster with apiserver="$ips" and endpoint="$endPoint
+  sudo kubeadm init --config kubeadm-config.yaml --apiserver-cert-extra-sans=$ips --pod-network-cidr=10.32.0.0/12 --control-plane-endpoint=$endPoint --upload-certs --certificate-key=$CERTKEY &> kube_out_cp.info
+fi
