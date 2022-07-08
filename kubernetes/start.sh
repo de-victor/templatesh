d5b251811ade6282f05b2a74f407901bad0239a4
Emmanuel Victor <2ndrunner@gmail.com>
2022-07-07 23:15:42 -0300

    init kubernate cp and podnetwork

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -1,20 +1,11 @@
-ips=$1
-endPoint=$2
+sudo kubeadm init --config kubeadm-config.yaml
+#sudo kubeadm init --apiserver-cert-extra-sans=$ips --pod-network-cidr=10.32.0.0/12 --control-plane-endpoint=$endPoint --upload-certs --certificate-key=$CERTKEY
 
-if [ -z $ips ] || [ -z $endPoint ] ; then
-  echo "Inform the ips and endPoint"
-  echo "ex: .\start.sh xx.xx.xx.xx,xx.xx.xx.xx zz.zz.zz.zz"
-else
-  CERTKEY=$(kubeadm certs certificate-key)
-  echo $CERTKEY &> certkey.key
-  echo $CERTKEY
-  echo "trying to setting up kubernet cluster with apiserver="$ips" and endpoint="$endPoint
-  sudo kubeadm init --config kubeadm-config.yaml
-  #sudo kubeadm init --apiserver-cert-extra-sans=$ips --pod-network-cidr=10.32.0.0/12 --control-plane-endpoint=$endPoint --upload-certs --certificate-key=$CERTKEY
-  
-  #after kubernetes init
-  mkdir -p $HOME/.kube
-  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-  sudo chown $(id -u):$(id -g) $HOME/.kube/config
-  kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')" --output=yaml
-fi
+#after kubernetes init
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')" --output=json
+
+#generate a new token
+sudo kubeadm token create
