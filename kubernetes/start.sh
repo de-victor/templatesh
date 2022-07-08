4bb8357d971578346f798dbccbd6a86f7ca187b3
Emmanuel Victor <2ndrunner@gmail.com>
2022-07-07 23:27:45 -0300

    clean code

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -1,11 +1,26 @@
-sudo kubeadm init --config kubeadm-config.yaml
-#sudo kubeadm init --apiserver-cert-extra-sans=$ips --pod-network-cidr=10.32.0.0/12 --control-plane-endpoint=$endPoint --upload-certs --certificate-key=$CERTKEY
+#! /bin/sh
 
-#after kubernetes init
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')" --output=json
+endPoint=$1
+file=kubeadm-config.yaml
 
-#generate a new token
-sudo kubeadm token create
+if [ -z $ips ] ; then
+  echo "Inform endPoint"
+  echo "ex: .\start.sh xx.xx.xx.xx"
+else
+  #modify config yaml
+  sed -i 's/controlPlaneEndpoint:.*/controlPlaneEndpoint:'$endPoint'/' $file
+
+  #start kubernet control panel
+  sudo kubeadm init --config kubeadm-config.yaml
+
+  #after kubernetes init
+  mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+  
+  #seting pods network
+  kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')" --output=json
+
+  #generate a new token
+  sudo kubeadm token create
+fi
