d475c6131e0bec034738d09f9192c268fbbe12a8
Emmanuel Victor <2ndrunner@gmail.com>
2022-07-08 22:20:10 -0300

    try to fix cp join

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -1,14 +1,16 @@
 #! /bin/sh
 
 endPoint=$1
+localApi=$2
 file=kubeadm-config.yaml
 
-if [ -z $endPoint ] ; then
-  echo "Inform endPoint"
-  echo "ex: .\start.sh 192.0.7.41"
+if [ -z $endPoint ] || [ -z $localApi ] ; then
+  echo "Inform endPoint and localApi"
+  echo "ex: .\start.sh 192.0.7.41 10.0.2.01"
 else
   #modify config yaml
   sed -i 's/controlPlaneEndpoint:.*/controlPlaneEndpoint: '$endPoint'/' $file
+  sed -i 's/advertiseAddress:.*/advertiseAddress: '$localApi'/' $file
 
   #start kubernet control panel
   sudo kubeadm init --config kubeadm-config.yaml --upload-certs
